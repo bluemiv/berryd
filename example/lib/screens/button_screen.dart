e2e8e8a31b89b0dfcd6b1f89b9bae44a38b87119
taehongkim <public.bluemiv@gmail.com>
2023-01-22 12:50:39 +0900

    feat: button 위젯 디렉토리 구조 변경 #1

---
+++ example/lib/screens/button_screen.dart
@@ -0,0 +1,61 @@
+import 'package:berryd/berryd.dart';
+import 'package:flutter/material.dart';
+
+class ButtonScreen extends StatelessWidget {
+  const ButtonScreen({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return BdLayout(
+      appBar: AppBar(
+          title: const BdText(
+        "Buttons",
+        color: Colors.white,
+      )),
+      padding: EdgeInsets.all(BdSpace.sm),
+      child: Wrap(
+        direction: Axis.vertical,
+        spacing: BdSpace.sm,
+        children: [
+          BdButton(
+            backgroundColor: BdBlack.color,
+            onTap: () {
+              print("clicked default button!");
+            },
+            child: Text(
+              "Default button",
+              style: TextStyle(color: Colors.white, fontSize: BdFontSize.md),
+            ),
+          ),
+          BdPrimaryButton(
+            label: "Primary button",
+            onTap: () {
+              print("clicked primary button!");
+            },
+          ),
+          BdSecondaryButton(
+            label: "Secondary button",
+            onTap: () {
+              print("clicked secondary button!");
+            },
+          ),
+          BdLinkButton(
+            label: "Link button",
+            onTap: () {
+              print("clicked link button!");
+            },
+          ),
+          BdGradientButton(
+            child: Text(
+              "Gradient button",
+              style: TextStyle(color: Colors.white, fontSize: BdFontSize.md),
+            ),
+            onTap: () {
+              print("clicked gradient button!");
+            },
+          ),
+        ],
+      ),
+    );
+  }
+}
--- lib/berryd.dart
+++ lib/berryd.dart
@@ -1,9 +1,14 @@
 library berryd;
 
-export 'components/button.dart';
-export 'components/gradient_button.dart';
-export 'components/link_button.dart';
-export 'components/primary_button.dart';
-export 'components/secondary_button.dart';
+export 'components/buttons/button.dart';
+export 'components/buttons/gradient_button.dart';
+export 'components/buttons/link_button.dart';
+export 'components/buttons/primary_button.dart';
+export 'components/buttons/secondary_button.dart';
+
+export 'components/typography/text.dart';
+
+export 'components/layouts/layout.dart';
+
 export 'core/color_service.dart';
 export 'core/constants.dart';
